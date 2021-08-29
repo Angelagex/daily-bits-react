e7ac354b9b5b52d6ded44d90103049e24766ae04
Felix <felix8517@gmail.com>
2021-08-29 12:49:50 -0500

    agregando alerts

--- src/components/pregunta/PreguntaSelec.jsx
+++ src/components/pregunta/PreguntaSelec.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import Swal from 'sweetalert2'
 import '../../styles/Styles.css';
 
 
@@ -19,7 +20,11 @@ const Avatar = styled.img`
     margin-top: 30px;
 `
 const PreguntaTexto = styled.h1`
-border-width: 2px;
+  margin: 20px;
+    display: flex;
+    flex-direction: row;
+    
+    font-size: 25px;
 `
 const PreguntaSelect = styled.div`
  
@@ -92,116 +97,164 @@ const Container = styled.label.attrs((/* props */) => ({ tabIndex: 0 }))`
         background-image: url("https://gejocad.github.io/DailyBits/img/radio-button-active.png");
         }
     `
+    
+    
+const Contenedor = styled.div`
+position:absolute;
+  align-items: center;
+ justify-content: space-between;
+ align-content: center;
+ transform: translateX(60px);
+ bottom: 25px;
+ 
+`
+
+
+const Button = styled.button`
+  position:absolute;
+  width: 328px;
+  height: 50px; 
+  color: var(--color-white);
+  bottom: 16px;
+  margin-top: 20px;
+  background: #D4CAF3;
+  border-radius: 16px;
+`
 
 
 const preguntasDB = 'https://gist.githubusercontent.com/gejocad/b07d390e21f55663e95c7ff741884414/raw/71ea594519317104f500611611a9837148f5fcf0/preguntasSelecDB.json';
 
 
- class PreguntaSelec extends Component {
+
+class PreguntaSelec extends Component {
 
   constructor() {
-    
+
     super();
     this.state = {
-        preguntas: [
-          {
-            id:  "",
-            avatar: "",
-            pregunta: "",
-            resCorrect: ""
-          }
-        ],
-        cuenta: 0
-      }
-      this.onValueChange = this.onValueChange.bind(this);
+      preguntas: [
+        {
+          id: "",
+          avatar: "",
+          pregunta: "",
+          resCorrect: ""
+        }
+      ],
+      cuenta: 0
+    }
+    this.onValueChange = this.onValueChange.bind(this);
     this.formSubmit = this.formSubmit.bind(this);
   }
-    onValueChange(event) {
-      this.setState({ 
-        selectedOption: event.target.value
+  onValueChange(event) {
+    this.setState({
+      btn: "",
+
+      selectedOption: event.target.value
+    });
+
+    this.activarComprobar();
+  }
+
+
+  componentDidMount() {
+
+    fetch(preguntasDB).then(res => res.json())
+      .then(res => {
+        this.setState({
+          preguntas: res.preguntas
+        }, this.siguientePregunta);
       });
+  }
+
+  siguientePregunta = () => {
+    const { preguntas } = this.state;
+
+    if (preguntas.length > 0) {
+      const cuenta = Math.floor(Math.random() * preguntas.length);
+      this.setState({
+        cuenta
+      })
     }
-  
+  }
 
-      componentDidMount() {
-  
-        fetch(preguntasDB).then(res => res.json())
-          .then(res => {
-            this.setState({
-              preguntas: res.preguntas
-            }, this.activador);
-        });
-      }
-
-      activador = () => {
-        const { preguntas } = this.state;
-        
-        if(preguntas.length > 0) {
-          const cuenta = Math.floor(Math.random() * preguntas.length);
-          this.setState({
-            cuenta
-          })
-        }
-      }
+  activarComprobar = () => {
+    let button = document.querySelector(".btn-active")
+    button.innerHTML = ''
+    button.innerHTML += `
+        <button id="run" type="submit" class="comprobar-active">COMPROBAR</button>
+      `
       
-      formSubmit(event) {
-        event.preventDefault();
-        
-       let prueba = localStorage.getItem("comprobar")
-       let prueba2 = this.state.selectedOption
-       console.log(prueba)
-       console.log(prueba2)
-       if (prueba === prueba2){console.log("aprobado camarada")}else{console.log("sorry bb")}
-      }
+      
+  }
 
-    render() {
-        const { preguntas, cuenta } = this.state;
-        const pregunta = preguntas[cuenta];
+  formSubmit(event) {
+    event.preventDefault();
+
+    let resCorrect = localStorage.getItem("resCorrect")
+    let resSelect = this.state.selectedOption
+    console.log(resCorrect)
+    console.log(resSelect)
+    if (resCorrect === resSelect) {  Swal.fire(
+      'Excelente!',
+      'Respuesta Correcta',
+      'success'
+    ) } else {  Swal.fire(
+      'Mal!',
+      'Respuesta Incorrecta',
+      'success'
+    ) }
+    this.siguientePregunta();
+    
+
+  }
 
 
-      function comprobarT(){
-        const prueba = pregunta.resCorrect
-        localStorage.setItem("comprobar", prueba)
-        
-        
-      }
+  render() {
+    const { preguntas, cuenta } = this.state;
+    const pregunta = preguntas[cuenta];
 
 
-        return (
-            <ContenedorPrincipal>
-                  <Pregunta>
-                    <Avatar src={pregunta.avatar} alt="calcelar" border="0" />
-                    <PreguntaTexto>{pregunta.pregunta}</PreguntaTexto>
-                </Pregunta>
-                <form onSubmit={this.formSubmit} onClick={comprobarT()}>
-                <Preguntas>
-                    <Container className="custom-radio-checkbox"><PreguntaSelect>{pregunta.res1}</PreguntaSelect>
-                        <Input className="custom-radio-checkbox__input" type="radio" name="answer" value={pregunta.res1} onChange={this.onValueChange}/>
-                        <Span className="custom-radio-checkbox__show custom-radio-checkbox__show--radio"/>
-                    </Container>
-                </Preguntas>
-                <Preguntas>
-                   <Container className="custom-radio-checkbox"><PreguntaSelect>{pregunta.res2}</PreguntaSelect>
-                        <Input className="custom-radio-checkbox__input" type="radio" name="answer" value={pregunta.res2} onChange={this.onValueChange}/>
-                        <Span className="custom-radio-checkbox__show custom-radio-checkbox__show--radio"/>
-                    </Container>
-                </Preguntas>
-                <Preguntas>
-                    <Container className="custom-radio-checkbox"><PreguntaSelect>{pregunta.res3}</PreguntaSelect>
-                        <Input className="custom-radio-checkbox__input" type="radio" name="answer" value={pregunta.res3} onChange={this.onValueChange}/>
-                        <Span className="custom-radio-checkbox__show custom-radio-checkbox__show--radio"/>
-                    </Container>
-                </Preguntas>
-                <div>
-                Selected option is : {this.state.selectedOption}
-              </div>
-              <button className="btn btn-default" type="submit" >
-                Submit
-              </button>
-                </form>
-        </ContenedorPrincipal>
-        )
+    function comprobarT() {
+      const resCorrect = pregunta.resCorrect
+      localStorage.setItem("resCorrect", resCorrect)
+
+
     }
+
+
+    return (
+      <ContenedorPrincipal>
+        <Pregunta>
+          <Avatar src={pregunta.avatar} alt="calcelar" border="0" />
+          <PreguntaTexto>{pregunta.pregunta}</PreguntaTexto>
+        </Pregunta>
+        <form onSubmit={this.formSubmit} onClick={comprobarT()}>
+          <Preguntas>
+            <Container className="custom-radio-checkbox"><PreguntaSelect>{pregunta.res1}</PreguntaSelect>
+              <Input className="custom-radio-checkbox__input" type="radio" name="answer" value={pregunta.res1} onChange={this.onValueChange} />
+              <Span className="custom-radio-checkbox__show custom-radio-checkbox__show--radio" />
+            </Container>
+          </Preguntas>
+          <Preguntas>
+            <Container className="custom-radio-checkbox"><PreguntaSelect>{pregunta.res2}</PreguntaSelect>
+              <Input className="custom-radio-checkbox__input" type="radio" name="answer" value={pregunta.res2} onChange={this.onValueChange} />
+              <Span className="custom-radio-checkbox__show custom-radio-checkbox__show--radio" />
+            </Container>
+          </Preguntas>
+          <Preguntas>
+            <Container className="custom-radio-checkbox"><PreguntaSelect>{pregunta.res3}</PreguntaSelect>
+              <Input className="custom-radio-checkbox__input" type="radio" name="answer" value={pregunta.res3} onChange={this.onValueChange} />
+              <Span className="custom-radio-checkbox__show custom-radio-checkbox__show--radio" />
+            </Container>
+          </Preguntas>
+          <Contenedor>
+            <Button className="btn-active" type="submit">COMPROBAR</Button>
+          </Contenedor>
+        </form>
+      </ContenedorPrincipal>
+
+    )
+  }
 }
 
 export default PreguntaSelec;
